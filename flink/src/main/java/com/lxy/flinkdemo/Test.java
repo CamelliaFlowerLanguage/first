f13bd5d724855ff0f69b936a168386bd3826c66e
zhubicheng <zhubicheng@lixiaoyun.com>
2023-04-27 13:55:23 +0800

    第二次修改

--- flink/src/main/java/com/lxy/flinkdemo/Test.java
+++ flink/src/main/java/com/lxy/flinkdemo/Test.java
@@ -9,14 +9,14 @@ import java.util.Properties;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        Properties kafkaProperties = new Properties();
-
-        FileInputStream in = new FileInputStream("src/main/resources/kafka.properties");
-        kafkaProperties.load(in);
-        String bootstrapServers = kafkaProperties.getProperty("bootstrapServers");
-        String topics = kafkaProperties.getProperty("topics");
-        String groupId = kafkaProperties.getProperty("groupId");
-        System.out.println(bootstrapServers);
+//        Properties kafkaProperties = new Properties();
+//
+//        FileInputStream in = new FileInputStream("src/main/resources/kafka.properties");
+//        kafkaProperties.load(in);
+//        String bootstrapServers = kafkaProperties.getProperty("bootstrapServers");
+//        String topics = kafkaProperties.getProperty("topics");
+//        String groupId = kafkaProperties.getProperty("groupId");
+//        System.out.println(bootstrapServers);
     }
 
 }
